7991f503fb46840fcc428d3d40d59fb0cc11a10c
Jun Zhou <zhoujun97099@gmail.com>
2022-11-29 09:11:41 +0100

    Project Creation

---
+++ app/src/main/java/com/dam/evaluaciont1_junz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dam.evaluaciont1_junz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
