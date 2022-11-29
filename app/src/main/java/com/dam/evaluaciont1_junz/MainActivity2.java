eb3158dccd70a8014e8cc833c4df947ea5bcbf24
Jun Zhou <zhoujun97099@gmail.com>
2022-11-29 11:46:42 +0100

    finished front-end second main activity

--- app/src/main/java/com/dam/evaluaciont1_junz/MainActivity2.java
+++ app/src/main/java/com/dam/evaluaciont1_junz/MainActivity2.java
@@ -2,21 +2,18 @@ package com.dam.evaluaciont1_junz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity2 extends AppCompatActivity {
+// Pantalla de Registro de Resultado
     Button btnRegistrar;
     Button btnConsultar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btnRegistrar = findViewById(R.id.btnRegistrar);
-        btnConsultar = findViewById(R.id.btnConsultar);
+        setContentView(R.layout.activity_main2);
+
 
     }
 }
