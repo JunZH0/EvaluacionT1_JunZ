b478a0a03a43278359a99e7fa20a52b64aec3edf
Jun Zhou <zhoujun97099@gmail.com>
2022-11-30 14:00:24 +0100

    fixed horizontal view in activity_main.xml, TODO clean method in MainActivity2.java

--- app/src/main/java/com/dam/evaluaciont1_junz/MainActivity2.java
+++ app/src/main/java/com/dam/evaluaciont1_junz/MainActivity2.java
@@ -2,18 +2,52 @@ package com.dam.evaluaciont1_junz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
 // Pantalla de Registro de Resultado
-    Button btnRegistrar;
-    Button btnConsultar;
+    Button fase1;
+    Button fase2;
+    Button btnGuardarRes;
+    Button btnLimpiarDatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        fase1 = findViewById(R.id.btnFase1);
+        fase2 = findViewById(R.id.btnFase2);
+        btnGuardarRes = findViewById(R.id.btnGuardarRes);
+        btnLimpiarDatos = findViewById(R.id.btnGuardarRes);
 
+        btnGuardarRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
 
+        btnLimpiarDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        /*
+        private void limpiarDatos() {
+            ViewGroup group = (ViewGroup)findViewById(R.id.your_group);
+            for (int i = 0, count = group.getChildCount(); i < count; ++i) {
+                View view = group.getChildAt(i);
+                if (view instanceof EditText) {
+                    ((EditText)view).setText("");
+                }
+            }
+        }
+        */
     }
 }
